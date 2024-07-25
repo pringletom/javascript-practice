9b933a26088973c361c4a1ddf5795e7a9f0bf1d0
michaeljgrant <mjg.grant.m@gmail.com>
2024-07-25 23:35:19 +1000

    add javascript tasks

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
-const myName1="Michael"
-let myName="Tom"
-console.log(myName1, myName)
-const myFunctionName=()=>{
-    console.log(myName)
-}
-myFunctionName()
+// hint this is a math function it just looks stupid
+const c = (o, n, n2) => {
+  return o == "*" ? n * n2 : "i don't know";
+};
 
-const plusNumbers=(a,b)=>{
-return a+b
-}
-console.log(plusNumbers(5,6))
+console.log(c("*", 5, 5));
+console.log(c("+", 5, 5));
+
+// Any data given to a function is called an argument
+// eg in python def function(arg1, arg2) -- the function has 2 arguments
+// Arguments can be any data type
+
+// Task 1
+// basic math functions - change python functions to Javascript
+
+// Task 2
+// return any string argument given backwards
+
+// Task 3
+// Take any string make it lowercase or uppercase based on the arg(s) given
+
+// Task 4
+// Return a random number between 1 and 100 every time the function is called
+// Bonus challenge: Rounded to 2 decimals
+
+// Task 5
+// What are the datatypes called in Javascript - list in comments next to Python equivalent
+
+// Bonus task - figure out how to get user input working
+// getArgs or something like that from the user
+
+// Lets do Javascript --> document & window changes --
+// Augment HTML
+// document.html - javascript alerts - to-do submission form
--- revision.py
+++ revision.py
@@ -1,16 +1,24 @@
-#def helloUser():
-   # user=input("what is your name?")
-   # return f"hello user}"
-#print(helloUser())
+# def helloUser():
+# user=input("what is your name?")
+# return f"hello user}"
+# print(helloUser())
 # #1: Calculator function
-def addition(x,y):
+def addition(x, y):
     return x + y
+
+
 def minus(x, y):
     return x - y
+
+
 def times(x, y):
     return x * y
+
+
 def divide(x, y):
     return x / y
+
+
 print("Welcome.")
 print("select a function.")
 print("1. Addition.")
@@ -19,13 +27,13 @@ print("3. Times.")
 print("4. Divide.")
 while True:
     choice = input("enter choice(1, 2, 3, 4):")
-    if choice in ('1','2','3','4'):
+    if choice in ('1', '2', '3', '4'):
         try:
-            num1= float(input("enter the first number " "")) #float to take whole and decimals
-            num2= float(input("enter the second number " "")) #converted string to int/F
+            num1 = float(input("enter the first number " ""))  # float to take whole and decimals
+            num2 = float(input("enter the second number " ""))  # converted string to int/F
         except ValueError:
             print("invaid input. Please enter a number.")
-    if  choice == '1':
+    if choice == '1':
         print(num1, "+", num2, "=", addition(num1, num2))
     elif choice == '2':
         print(num1, "-", num2, "=", minus(num1, num2))
@@ -33,32 +41,32 @@ while True:
         print(num1, "*", num2, "=", times(num1, num2))
     elif choice == '4':
         print(num1, "/", num2, "=", divide(num1, num2))
-      
+
     next_calculation = input("Let's do next calculation? (yes/no): ")
     if next_calculation == "no":
-          break
+        break
 else:
     print("Invalid Input")
 
 # #2: Function to check whether numbers are odd or even
 number = int(input("Enter an integer number."))
-remainder = number % 2 #modulus operator find remainder
+remainder = number % 2  # modulus operator find remainder
 if (remainder == 0):
     print(number, "is an even Number")
 else:
     print(number, "is an odd Number")
 
-#while loop and python list for yes/no questions
+# while loop and python list for yes/no questions
 yesResponse = {'you betcha', 'yes', 'Yes', 'yeah', 'y', 'ye', 'Ye', 'Y', 'Yeah', 'Nah yeah'}
 noResponse = {'nah', 'no', 'No', 'N', 'n', 'Nah', 'yeah nah'}
 sports = {"football": "football is famous because of Ronaldo and Messi.",
-          "rugby": "rugby is played within 80 minutes with two teams of 15.", 
-          "padel": "padel is the fasted growing racket sport.", 
+          "rugby": "rugby is played within 80 minutes with two teams of 15.",
+          "padel": "padel is the fasted growing racket sport.",
           "squash": "squash is played by hitting a ball against a wall.",
-          "tennis": "tennis is played over a fence with two players.", 
+          "tennis": "tennis is played over a fence with two players.",
           "cricket": "cricket is famous because of the ashes.",
           "dodgeball": "if you can dodge a wrench..."
-        }
+          }
 while True:
     response = input("would you like to learn about a sport? (yes or no):")
     if response in yesResponse:
@@ -66,8 +74,8 @@ while True:
             print(f"{index}: {value}")
         sport_choice = input("which sport would you like to learn about?")
     elif response in noResponse:
-            print("okily dokily!")
-            break 
+        print("okily dokily!")
+        break
     if sport_choice in sports:
         print(sports[sport_choice])
     else:
